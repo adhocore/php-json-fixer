593b217ab6a4ca808982a8c8396fbc04edfd2892
Jitendra Adhikari <jiten.adhikary@gmail.com>
2018-07-21 06:05:44 +0000

    Apply fixes from StyleCI

--- tests/FixerTest.php
+++ tests/FixerTest.php
@@ -22,93 +22,93 @@ class FixerTest extends \PHPUnit\Framework\TestCase
     public function theTests()
     {
         return [[
-            'json' => '',
+            'json'   => '',
             'expect' => '',
         ], [
-            'json' => '"',
+            'json'   => '"',
             'expect' => '""',
         ], [
-            'json' => '"a"',
+            'json'   => '"a"',
             'expect' => '"a"',
         ], [
-            'json' => 'true',
+            'json'   => 'true',
             'expect' => 'true',
         ], [
-            'json' => 'false',
+            'json'   => 'false',
             'expect' => 'false',
         ], [
-            'json' => 'null',
+            'json'   => 'null',
             'expect' => 'null',
         ], [
-            'json' => 'fal',
+            'json'   => 'fal',
             'expect' => 'false',
         ], [
-            'json' => 't',
+            'json'   => 't',
             'expect' => 'true',
         ], [
-            'json' => 'nu',
+            'json'   => 'nu',
             'expect' => 'null',
         ], [
-            'json' => '{',
+            'json'   => '{',
             'expect' => '{}',
         ], [
-            'json' => '[',
+            'json'   => '[',
             'expect' => '[]',
-        ],[
-            'json' => '[{',
+        ], [
+            'json'   => '[{',
             'expect' => '[{}]',
         ], [
-            'json' => '[1',
+            'json'   => '[1',
             'expect' => '[1]',
         ], [
-            'json' => '["',
+            'json'   => '["',
             'expect' => '[""]',
         ], [
-            'json' => '[1,',
+            'json'   => '[1,',
             'expect' => '[1]',
         ], [
-            'json' => '[1,{',
+            'json'   => '[1,{',
             'expect' => '[1,{}]',
         ], [
-            'json' => '["a',
+            'json'   => '["a',
             'expect' => '["a"]',
         ], [
-            'json' => '["b,',
+            'json'   => '["b,',
             'expect' => '["b,"]',
         ], [
-            'json' => '["b",{"',
+            'json'   => '["b",{"',
             'expect' => '["b",{"":null}]',
         ], [
-            'json' => '["b",{"a',
+            'json'   => '["b",{"a',
             'expect' => '["b",{"a":null}]',
         ], [
-            'json' => '["b",{"a":',
+            'json'   => '["b",{"a":',
             'expect' => '["b",{"a":null}]',
         ], [
-            'json' => '["b",{"a":[t',
+            'json'   => '["b",{"a":[t',
             'expect' => '["b",{"a":[true]}]',
         ], [
-            'json' => '{"a":2',
+            'json'   => '{"a":2',
             'expect' => '{"a":2}',
         ], [
-            'json' => '{"',
+            'json'   => '{"',
             'expect' => '{"":null}',
         ], [
-            'json' => '{"a":1.2,',
+            'json'   => '{"a":1.2,',
             'expect' => '{"a":1.2}',
         ], [
-            'json' => '{"a":"',
+            'json'   => '{"a":"',
             'expect' => '{"a":""}',
         ], [
-            'json' => '{"a":[',
+            'json'   => '{"a":[',
             'expect' => '{"a":[]}',
         ], [
-            'json' => '{"a":"b","b":["',
+            'json'   => '{"a":"b","b":["',
             'expect' => '{"a":"b","b":[""]}',
         ], [
-            'json' => '{"a":"b","b":[t',
+            'json'   => '{"a":"b","b":[t',
             'expect' => '{"a":"b","b":[true]}',
-        ]
+        ],
         ];
     }
 }
