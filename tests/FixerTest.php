d013deab549a20acd3253111b6e65b3a9445dba1
Jitendra Adhikari <jiten.adhikary@gmail.com>
2018-07-21 14:34:33 +0700

    test: more test cases

--- tests/FixerTest.php
+++ tests/FixerTest.php
@@ -19,6 +19,38 @@ class FixerTest extends \PHPUnit\Framework\TestCase
         $this->assertSame($expect, static::$fixer->fix($json), $msg);
     }
 
+    public function test_invalid_literal()
+    {
+        $this->assertSame('{"a" : invalid', static::$fixer->fix('{"a" : invalid', true));
+        $this->assertSame(' hmm ', static::$fixer->fix(' hmm ', true));
+    }
+
+    public function test_ws()
+    {
+        $this->assertSame('{ "a"  :null}', static::$fixer->fix('{ "a"  :'));
+        $this->assertSame("\n [{}]", static::$fixer->fix("\n [{,"));
+    }
+
+    public function test_custom_missing()
+    {
+        $this->assertSame('{"a":false}', static::$fixer->fix('{"a', false, 'false'));
+        $this->assertSame('{"a":true}', static::$fixer->fix('{"a":', false, 'true'));
+    }
+
+    public function test_fail_silent()
+    {
+        $this->assertSame('{"a"}', static::$fixer->fix('{"a"}', true));
+    }
+
+    /**
+     * @expectedException \RuntimeException
+     * @expectedExceptionMessage Could not fix JSON
+     */
+    public function test_fail_throws()
+    {
+        static::$fixer->fix('{,"a');
+    }
+
     public function theTests()
     {
         return [[
@@ -54,6 +86,12 @@ class FixerTest extends \PHPUnit\Framework\TestCase
         ], [
             'json'   => '[',
             'expect' => '[]',
+        ], [
+            'json'   => '12.34',
+            'expect' => '12.34',
+        ], [
+            'json'   => '"str',
+            'expect' => '"str"',
         ], [
             'json'   => '[{',
             'expect' => '[{}]',
@@ -108,6 +146,9 @@ class FixerTest extends \PHPUnit\Framework\TestCase
         ], [
             'json'   => '{"a":"b","b":[t',
             'expect' => '{"a":"b","b":[true]}',
+        ], [
+            'json'   => '[ {"id":1, "data": []}, {"id":2, "data": [',
+            'expect' => '[ {"id":1, "data": []}, {"id":2, "data": []}]',
         ],
         ];
     }
